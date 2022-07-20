70fa8d870b34bb137da2d97600ac0b90c8690327
hugues <mihin.aime@gmail.com>
2022-07-20 14:39:12 +0000

    change admin picture

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -47,7 +47,7 @@
             <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">{{ Auth::user()->firstname }}</span><span class="user-status">@if (Auth::user()->roleId == 1)
                         Super administrateur @else Administrateur @endif</span></div><span class="avatar">
-                        <img class="round" src="{{ asset('dashboard/app-assets//images/portrait/small/avatar.png')}}" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                        <img class="round" src="{{ asset('dashboard/app-assets/images/avatars/avatar.png')}}" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="me-50" data-feather="user"></i> Profile</a>
                     <div class="dropdown-divider"></div>
